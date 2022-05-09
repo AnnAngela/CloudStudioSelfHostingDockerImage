4fe80cf34a89c4aed412ec1843996b326be37207
AnnAngela <naganjue@vip.qq.com>
2022-05-09 10:08:38 +0800

    test

--- sh/nodejs.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-wget https://helper.annangela.cn/_docker/n -O /tmp/n
-bash /tmp/n lts
-rm /tmp/n
--- sh/npm.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-echo "export N_PRESERVE_NPM=1" | tee -a /root/.bashrc
-
-npm install npm n -g
-rm -rf /root/.npm
-mkdir -p /root/.npm
